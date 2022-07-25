57f86cfee8ad9e8ac31004b4fa5b0046044f1e83
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-26 00:58:58 +0300

    completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordInvalid,with: :render_record_invalid_error
   
   def show
     author = Author.find(params[:id])
@@ -7,7 +9,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
@@ -17,5 +19,13 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  def render_record_invalid_error(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end 
+
+  # def render_record_not_found_error
+  #   render json: {error: "Author not Found"},status: :not_found
+  # end 
   
 end
